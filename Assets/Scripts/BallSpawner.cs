b637f7e1d89b609f4ec19b84d76a77394d7acd46
Gustav <sir.gustav.the.coder@gmail.com>
2017-08-27 09:31:52 +0200

    added stuff

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour {
+	public Rigidbody2D BallToSpawn;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if( Input.GetKeyDown(KeyCode.LeftControl) ) 
+		{
+			var ball = GameObject.Instantiate(BallToSpawn);
+			ball.transform.position = this.transform.position + new Vector3(0, 0.5f, 0);
+			ball.AddForce(new Vector2(0, 100));
+		}
+	}
+}
---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+	Rigidbody2D rb;
+	float pos;
+	float y;
+
+	public float Speed = 3;
+
+	void Start () {
+		this.rb = this.GetComponent<Rigidbody2D>();
+		this.pos = this.rb.position.x;
+		this.y = this.rb.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dx = 0;
+		dx = Input.GetAxis("Horizontal");
+
+		this.pos += dx * Time.deltaTime * this.Speed;
+		this.rb.MovePosition(new Vector2(this.pos, this.y));
+	}
+}
