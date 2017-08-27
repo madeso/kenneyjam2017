02ffe9a79d31777168fc6f3ea084485233139511
Gustav <sir.gustav.the.coder@gmail.com>
2017-08-27 10:11:22 +0200

    more stuff

---
+++ Assets/Scripts/DestroyTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyTile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		print(coll.gameObject.tag);
+		if(coll.gameObject.tag == "Ball") {
+			Destroy(this.gameObject);
+		}
+	}
+}
